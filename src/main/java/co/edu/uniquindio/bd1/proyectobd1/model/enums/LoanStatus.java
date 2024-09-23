48f3a9f76a006a20aa3629f32f530b76bbe4982d
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2024-09-22 19:14:17 -0500

    Model creation (Enums)

---
+++ src/main/java/co/edu/uniquindio/bd1/proyectobd1/model/enums/LoanStatus.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.bd1.proyectobd1.model.enums;
+
+public enum LoanStatus {
+    PENDING, UNDERSTUDY, APPROVED, FAILED;
+}
---
+++ src/main/java/co/edu/uniquindio/bd1/proyectobd1/model/enums/UserRole.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.bd1.proyectobd1.model.enums;
+
+public enum UserRole {
+    OPERATOR, ADMINISTRATIVE, EXECUTIVE, TREASURY, OTHERS;
+}
